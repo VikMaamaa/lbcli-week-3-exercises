707804a389cc23b73f2f9fe035328291085d57d9
VikMaamaa <maamaavictor@yahoo.com>
2026-04-01 16:20:38 +0100

    total output value of the partially signed transaction in satoshis

--- submission/07.sh
+++ submission/07.sh
@@ -1,2 +1,5 @@
 # What is the total output value of this partially signed transaction in satoshis
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+#!/bin/bash
+txn="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
+bitcoin-cli -regtest -named decodepsbt psbt=$txn | jq -r '.tx.vout[] | .value' | awk '{s+=$1} END {print s*100000000}'
