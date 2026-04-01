35e96d6851aa830b883df1be442858ce8c6f8b2b
VikMaamaa <maamaavictor@yahoo.com>
2026-04-01 15:50:46 +0100

    task 1 and 2 fix

--- submission/01.sh
+++ submission/01.sh
@@ -1,2 +1,3 @@
 # Create a wallet with the name "builderswallet".
+#!/bin/bash
 bitcoin-cli -regtest -named createwallet wallet_name="builderswallet"
--- submission/02.sh
+++ submission/02.sh
@@ -1,4 +1,5 @@
 # Create a native segwit address and get the public key from the address.
+#!/bin/bash
 Address=$(bitcoin-cli -regtest -rpcwallet=builderswallet -named getnewaddress)
 Pubkey=$(bitcoin-cli -regtest -rpcwallet=builderswallet -named getaddressinfo address="$Address" | jq -r '.pubkey')
 echo $Pubkey
