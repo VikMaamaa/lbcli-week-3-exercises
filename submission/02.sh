e32f10d5b6d596e782537a9e34596857541a5408
VikMaamaa <maamaavictor@yahoo.com>
2026-04-01 16:56:55 +0100

    chore: comments update

--- submission/02.sh
+++ submission/02.sh
@@ -1,5 +1,8 @@
 # Create a native segwit address and get the public key from the address.
 #!/bin/bash
+# Generate a new Bitcoin address
 Address=$(bitcoin-cli -regtest -rpcwallet=builderswallet -named getnewaddress)
+# Retrieve the public key associated with that new address
 Pubkey=$(bitcoin-cli -regtest -rpcwallet=builderswallet -named getaddressinfo address="$Address" | jq -r '.pubkey')
+# Print the extracted public key to the terminal
 echo $Pubkey
--- submission/06.sh
+++ submission/06.sh
@@ -1,5 +1,7 @@
 # What is the hash of this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
 #!/bin/bash
+# Define the Partially Signed Bitcoin Transaction (PSBT)
 txn="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
+# Decode the PSBT and extract the transaction hash (txid)
 bitcoin-cli -regtest -named decodepsbt psbt=$txn | jq -r '.tx.txid'
